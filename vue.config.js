6e5c2271a133d1f25775798c3b0026d96a983070
kun.yang <kun.yang@kunyangdeMacBook-Pro.local>
2024-12-10 11:07:54 +0800

    build: 添加静态部署页面文件

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
+
+const path = require('path')
+
 module.exports = {
+  outputDir: path.resolve(__dirname, './root'),
   css: {
     loaderOptions: {
       sass: {
         data: `@import "@/styles/main.scss";`
       }
     }
-  }
+  },
 };
