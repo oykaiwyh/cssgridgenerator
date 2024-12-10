7a0edbd7ea82aafe6f452dd8bc471b4d1a6168c4
kun.yang <kun.yang@kunyangdeMacBook-Pro.local>
2024-12-10 11:15:58 +0800

    build: 修改静态部署页面配置

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@
 const path = require('path')
 
 module.exports = {
-  outputDir: path.resolve(__dirname, './root'),
+  outputDir: path.resolve(__dirname, './docs'),
+  publicPath: '/cssgridgenerator',
   css: {
     loaderOptions: {
       sass: {
